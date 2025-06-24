f65338b2270dcba14afacc72e1995d6e1533a0ca
lutinglt <lutinglt@users.noreply.github.com>
2025-06-24 20:48:27 +0800

    添加版本脚本

---
+++ scripts/version.cjs
@@ -0,0 +1,8 @@
+const fs = require('fs');
+const path = require('path');
+
+const pkgPath = path.join(__dirname, '..', 'package.json');
+const pkg = JSON.parse(fs.readFileSync(pkgPath));
+
+const version = pkg.version;
+console.log(version);
