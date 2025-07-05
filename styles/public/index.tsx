ce1bc3b762c717c7b4e274e4f6e1e6a9bd53ef97
lutinglt <lutinglt@users.noreply.github.com>
2025-07-05 21:59:28 +0800

    fix: radius 导入顺序

--- styles/public/index.tsx
+++ styles/public/index.tsx
@@ -1,3 +1,7 @@
+// organize-imports-ignore
+// tslint:disable:ordered-imports
+// 组件导入有顺序, 禁止插件优化
+import "./radius"; // 圆角, 此样式为基础样式, 确保在其他样式之前导入
 import "./button"; // 按钮
 import "./chroma"; // 代码高亮
 import "./codemirror"; // codemirror 样式
@@ -5,6 +9,5 @@ import "./dropdown"; // 下拉框
 import "./input"; // 输入框
 import "./label"; // 标签
 import "./modal"; // 弹窗
-import "./radius"; // 圆角
 import "./svg"; // svg 图标
 import "./tippy"; // 提示框
