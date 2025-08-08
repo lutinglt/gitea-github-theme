2338383f768a81dbaf1f46d6d49ec85620eab999
lutinglt <lutinglt@users.noreply.github.com>
2025-08-08 21:40:21 +0800

    chroma变量和修复注册页导航栏

--- styles/public/chroma.ts
+++ styles/public/chroma.ts
@@ -2,278 +2,45 @@ import { css } from "src/types/vars";
 
 // [TODO] 先重构为 Gitea 变量, 或者本主题的变量, 再建立 GitHub 的颜色映射
 
-export const chroma = css`
+// 取自 https://github.com/go-gitea/gitea/blob/main/web_src/css/chroma/base.css
+export const chromaBase = css`
   .chroma {
-    background-color: var(--color-code-bg);
-
+    // LineTableTD
     .lntd {
       vertical-align: top;
-      border: 0;
-      margin: 0;
       padding: 0;
+      margin: 0;
+      border: 0;
     }
-
+    // LineTable
     .lntable {
       border-spacing: 0;
+      padding: 0;
+      margin: 0;
       border: 0;
       width: auto;
-      margin: 0;
-      padding: 0;
-      display: block;
       overflow: auto;
-    }
-
-    .hl {
-      width: 100%;
       display: block;
     }
-
+    // LineHighlight
+    .hl {
+      display: block;
+      width: 100%;
+    }
+    // LineNumbersTable
     .lnt,
+    // LineNumbers
     .ln {
       margin-right: 0.4em;
       padding: 0 0.4em;
     }
-
+    // GenericStrong
     .gs {
       font-weight: var(--font-weight-semibold);
     }
-
+    // GenericUnderline
     .gl {
       text-decoration: underline;
-    }
-
-    .bp {
-      color: #fabd2f;
-    }
-
-    .c,
-    .c1,
-    .ch,
-    .cm {
-      color: #777e94;
-    }
-
-    .cp {
-      color: #8ec07c;
-    }
-
-    .cpf {
-      color: #79c0ff;
-    }
-
-    .cs {
-      color: #9075cd;
-    }
-
-    .dl {
-      color: #79c0ff;
-    }
-
-    .gd {
-      color: #fff;
-      background-color: #5f3737;
-    }
-
-    .ge {
-      color: #ddee30;
-    }
-
-    .gh {
-      color: #ffaa10;
-    }
-
-    .gi {
-      color: #fff;
-      background-color: #3a523a;
-    }
-
-    .go {
-      color: #777e94;
-    }
-
-    .gp {
-      color: #ebdbb2;
-    }
-
-    .gr {
-      color: #f43;
-    }
-
-    .gs {
-      color: #ebdbb2;
-    }
-
-    .gt {
-      color: #7ee787;
-    }
-
-    .gu {
-      color: #a5d6ff;
-    }
-
-    .il {
-      color: #79c0ff;
-    }
-
-    .k {
-      color: #ff7b72;
-    }
-
-    .kc {
-      color: #79c0ff;
-    }
-
-    .kd {
-      color: #ff7b72;
-    }
-
-    .kn {
-      color: #ff7b72;
-    }
-
-    .kp {
-      color: #5f8700;
-    }
-
-    .kr {
-      color: #7ee787;
-    }
-
-    .kt {
-      color: #ff7b72;
-    }
-
-    .m,
-    .mb,
-    .mf,
-    .mh,
-    .mi,
-    .mo {
-      color: #79c0ff;
-    }
-
-    .n {
-      color: #c9d1d9;
-    }
-
-    .na {
-      color: #d2a8ff;
-    }
-
-    .nb {
-      color: #a5d6ff;
-    }
-
-    .nc {
-      color: #e6edf3;
-    }
-
-    .nd {
-      color: #79c0ff;
-    }
-
-    .ne {
-      color: #7ee787;
-    }
-
-    .nf,
-    .ni {
-      color: #d2a8ff;
-    }
-
-    .nl {
-      color: #7ee787;
-    }
-
-    .nn {
-      color: #e6edf3;
-    }
-
-    .no {
-      color: #79c0ff;
-    }
-
-    .nt {
-      color: #7ee787;
-    }
-
-    .nv {
-      color: #ebdbb2;
-    }
-
-    .nx {
-      color: #b6bac5;
-    }
-
-    .o {
-      color: #7ee787;
-    }
-
-    .ow {
-      color: #5f8700;
-    }
-
-    .p {
-      color: #d2d4db;
-    }
-
-    .s,
-    .s1,
-    .s2 {
-      color: #a5d6ff;
-    }
-
-    .sa {
-      color: #79c0ff;
-    }
-
-    .sb {
-      color: #a5d6ff;
-    }
-
-    .sc {
-      color: #79c0ff;
-    }
-
-    .sd {
-      color: #777e94;
-    }
-
-    .se {
-      color: #7ee787;
-    }
-
-    .sh {
-      color: #79c0ff;
-    }
-
-    .si {
-      color: #ffaa10;
-    }
-
-    .sr {
-      color: #9075cd;
-    }
-
-    .ss {
-      color: #7ee787;
-    }
-
-    .sx {
-      color: #ffaa10;
-    }
-
-    .vc {
-      color: #7ee787;
-    }
-
-    .vg,
-    .vi {
-      color: #ffaa10;
-    }
-
-    .w {
-      color: #7f8699;
     }
   }
 `;
