7039041d9de9ecf53c3d059202534dbfb3f47ac9
lutinglt <lutinglt@users.noreply.github.com>
2025-08-13 11:34:48 +0800

    同步编辑器字体大小

--- styles/components/editor.ts
+++ styles/components/editor.ts
@@ -2,15 +2,19 @@ import { css, themeVars } from "src/types/vars";
 
 export const monaco = css`
   .monaco-editor {
-    font-size: 12px;
     --vscode-editor-background: ${themeVars.color.body} !important;
     --vscode-editorGutter-background: ${themeVars.color.body} !important;
+    .monaco-mouse-cursor-text {
+      font-size: 12px !important;
+    }
   }
   // 滚动时固定在顶部的行
   .monaco-editor .sticky-widget {
     background: ${themeVars.color.body} !important;
-    border-bottom: 0.5px solid ${themeVars.color.secondary.self};
     box-shadow: 0 1px 0 ${themeVars.color.secondary.self} !important;
     width: 100% !important;
+    .sticky-line-content {
+      font-size: 12px !important;
+    }
   }
 `;
