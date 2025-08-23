09a4bbdc071a6c05ac086b74f0f82f8999772754
lutinglt <lutinglt@users.noreply.github.com>
2025-08-23 11:55:33 +0800

    取消修改编辑器字体大小, 避免光标错位

--- styles/components/editor.ts
+++ styles/components/editor.ts
@@ -4,17 +4,11 @@ export const monaco = css`
   .monaco-editor {
     --vscode-editor-background: ${themeVars.color.body} !important;
     --vscode-editorGutter-background: ${themeVars.color.body} !important;
-    .monaco-mouse-cursor-text {
-      font-size: 12px !important;
-    }
     // 滚动时固定在顶部的行
     .sticky-widget {
       background: ${themeVars.color.body} !important;
       box-shadow: 0 1px 0 ${themeVars.color.secondary.self} !important;
       width: 100% !important;
-      .sticky-line-content {
-        font-size: 12px !important;
-      }
     }
   }
 `;
