5d96a6896dc6a61ed04b8a179f388446dd150909
lutinglt <lutinglt@users.noreply.github.com>
2025-09-11 11:48:35 +0800

    修复display主题下差异对比展开折叠代码按钮颜色

--- src/core/display.ts
+++ src/core/display.ts
@@ -22,6 +22,10 @@ export function display2GithubColor(
 ): GithubColor {
   return {
     ...baseGithubColor,
+    diffBlob: {
+      ...baseGithubColor.diffBlob,
+      hunkNum: { bgColor: { rest: soft ? displayColor.num2 : displayColor.num1 } },
+    },
     fgColor: {
       ...baseGithubColor.fgColor,
       accent: soft ? displayColor.num7 : displayColor.num6,
